57fcf86ff22ac522c497090d521e4b268daa3f98
aryangurau <aryangurau143@gmail.com>
2024-09-07 11:15:00 +0545

    learned logical operation in react

--- React/src/Logical.jsx
+++ React/src/Logical.jsx
@@ -4,15 +4,23 @@
 // };  // using logical operator
 
 const Logical = ({ cars }) => {
+
+  let bmw = cars.filter((a)=>a.startsWith("K"))
   return (
+   <>
+<h1> {cars}</h1>
+   
     <div>
      
       {cars.length > 0 ? (
-        <div>{cars.toString()}</div>
+    
+        <div>filtered:{bmw}</div>
+       
       ) : (
         <div>cars not found</div>
       )}
     </div>
+    </>
   );
  };
 export default Logical
